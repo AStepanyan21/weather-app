72feb19fd28c121ac9b9821d967b65ff6b44509f
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-08-22 22:04:55 +0400

    add to store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
 import './index.css'
 import App from './App'
-import rootReducer from  './store/reducers/rootReducer'
 import * as serviceWorker from './serviceWorker'
+import {store} from './store'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 const app = (
   <Provider store = {store}>
